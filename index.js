18be7bb7aa3ec56a2f48ef3bae2e2f0c0bbb5f07
sohanbd413-prog <sohanbd413@gmail.com>
2026-05-18 00:35:34 +0600

    all tutors added

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
-const uri = process.env.MONGO_URI;
-
 const app = express();
+const uri = process.env.MONGO_URI;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
 const client = new MongoClient(uri, {
@@ -17,9 +20,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
     await client.connect();
-   
+    const database = client.db("mentorify");
+    const usersCollection = database.collection("users");
+    // all tutors
+    app.get("/tutors", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const tutors = await cursor.toArray();
+      res.send(tutors);
+    });
+
+    // add tutors
+    app.post("/tutors", async (req, res) => {
+      const tutor = req.body;
+      const result = await usersCollection.insertOne(tutor);
+      res.send(result);
+    });
+
+    // limited tutors
+    app.get("/tutors/limited", async (req, res) => {
+      const cursor = usersCollection.find({}).limit(6);
+      const tutors = await cursor.toArray();
+      res.send(tutors);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
